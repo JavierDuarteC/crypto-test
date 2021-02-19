0b8d3cb66d585a20efc7cb68cb8583e9564e0365
DESKTOP-I8AOQIP\AMINI <javier.duarte@areamovil.com.co>
2021-02-19 17:17:41 -0500

    lazy loading more currency table

--- frontend/crypto-test/src/app/currency-table/currency-table.component.ts
+++ frontend/crypto-test/src/app/currency-table/currency-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { bncApiService } from '../services/bnc.service';
 import { Currency } from '../classes/currency';
+import { Target } from '@angular/compiler';
 
 export interface PeriodicElement {
   name: string,
@@ -30,31 +31,60 @@ export class CurrencyTableComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'price', 'isCrypto', 'convert'];
   dataSource: Currency[] = [];
+  currenciesData: Currency[] = [];
   from = 0;
   to = 0;
   limit = 0;
+  _loading = false;
 
   constructor(private _bncApiService: bncApiService) { }
 
   async ngOnInit(): Promise<void> {
     await this._bncApiService.setToken();
-    var currenciesData = (await this._bncApiService.getCurrencies().toPromise()).content;
-    this.limit = currenciesData.length;
-    if (this.limit > 0 && this.limit < 21) {
-      this.to = this.limit;
-    } else if (this.limit > 0 && this.limit > 20) {
-      this.to = 20;
-    }
-    for (let i = this.from; i < this.to; i++) {
-      var data = await this._bncApiService.getPrice(currenciesData[i].id).toPromise();
-      //console.log(data);
-      if ("content" in data) {
-        if (data.content.length > 0) {
-          currenciesData[i].price = data.content[0].price;
-        }
+    this.currenciesData = (await this._bncApiService.getCurrencies().toPromise()).content;
+    this.limit = this.currenciesData.length;
+    this.getPricesPaginated();
+  }
+
+  onTableScroll(e: any) {
+    if (this._loading) {
+      console.log("Loading...");
+    } else {
+      const tableViewHeight = e.target.offsetHeight; // viewport
+      const tableScrollHeight = e.target.scrollHeight; // length of all table
+      const scrollLocation = e.target.scrollTop; // how far user scrolled
+
+      // If the user has scrolled within 200px of the bottom, add more data
+      const buffer = 800;
+      const limit = tableScrollHeight - tableViewHeight - buffer;
+      if (scrollLocation > limit) {
+        console.log("Load More");
+        this.getPricesPaginated();
       }
-      this.dataSource = currenciesData.slice(0,i+1);
     }
   }
 
+  async getPricesPaginated(): Promise<void> {
+    this._loading = true;
+    if (this.to === this.limit) {
+      console.log("Finished");
+    } else if (this.limit > 0 && this.limit < 20) {
+      this.to = this.limit;
+    } else if (this.limit > 0 && this.limit > this.to) {
+      this.from = this.to;
+      this.to = this.to+20;
+    }
+    for (let i = this.from; i < this.to; i++) {
+      var data = await this._bncApiService.getPrice(this.currenciesData[i].id).toPromise();
+      //console.log(data);
+      if ("content" in data) {
+        if (data.content.length > 0) {
+          this.currenciesData[i].price = data.content[0].price;
+        }
+      }
+    }
+    this.dataSource = this.currenciesData.slice(0, this.to);
+    console.log("loaded 20 more");
+    this._loading = false;
+  }
 }
